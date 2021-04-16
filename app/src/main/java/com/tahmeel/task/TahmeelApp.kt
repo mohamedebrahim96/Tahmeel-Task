fe82f7d5272dd5b4b518d87d22265d7a5d56c4b8
Mohamed Ebrahim <ebrahimm131@gmail.com>
2021-04-16 12:06:18 +0400

    add dagger-hilt libarary

---
+++ app/src/main/java/com/tahmeel/task/TahmeelApp.kt
@@ -0,0 +1,14 @@
+package com.tahmeel.task
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by @mohamedebrahim96 on 16,April,2021
+ * ShopiniWorld, Inc
+ * ebrahimm131@gmail.com
+ */
+
+
+@HiltAndroidApp
+class TahmeelApp : Application()
