8fea937d372e3474fa4b7e7f791b9344bcab30a5
Mohamed Ebrahim <ebrahimm131@gmail.com>
2021-04-16 18:45:34 +0400

    add TahmeelClient and TahmeelService for network calls also add retrofit

--- app/src/main/java/com/tahmeel/task/repository/MainRepository.kt
+++ app/src/main/java/com/tahmeel/task/repository/MainRepository.kt
@@ -1,7 +1,11 @@
 package com.tahmeel.task.repository
 
 import androidx.annotation.WorkerThread
-import androidx.lifecycle.Transformations.map
+import com.skydoves.sandwich.onError
+import com.skydoves.sandwich.onException
+import com.skydoves.sandwich.suspendOnSuccess
+import com.skydoves.whatif.whatIfNotNull
+import com.tahmeel.task.network.TahmeelClient
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
@@ -27,17 +31,17 @@ class MainRepository @Inject constructor(
         var orders = tahmeelDao.getPokemonList(page)
         if (orders.isEmpty()) {
 
-            val response = tahmeelClient.fetchPokemonList(page = page)
+            val response = tahmeelClient.fetchPendingOrdersList(page = page)
             response.suspendOnSuccess {
                 data.whatIfNotNull { response ->
-                    orders = response.results
-                    orders.forEach { order -> order.page = page }
+                    orders = response.pendingOrders
+                    //orders.forEach { order -> order.page = page }
                     tahmeelDao.insertPokemonList(orders)
                     emit(tahmeelDao.getAllPokemonList(page))
                 }
             }
                 .onError {
-                    map(ErrorResponseMapper) { onError("[Code: $code]: $message") }
+                    onError("[Code: ")
                 }
                 .onException { onError(message) }
         } else {
